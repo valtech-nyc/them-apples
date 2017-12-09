d0b70f5c81451fb9b26b3620fb6433128e098c2d
mAAdhaTTah <jamesorodig@gmail.com>
2017-12-09 15:01:42 -0500

    Serve assets thru webpack-dev-middleware
    Configured to serve the assets and render the backend through
    express. We're not using a view library, instead just sending a string
    at the root. This should be enough for now, but we can configure
    SSR with React if we want. Given that this is a game, I'm not sure
    that's valuable here.
    
    HMR is enabled, but App.js will need to be wrapped with
    `react-hot-loader` to function correctly for React. For now, it does
    a page refresh but does inject the styles at runtime.
    
    The express generator is used as the base for this server. We also
    kill off the polyfills, as we're only targeting new browsers. We will also
    update the babel configuration to do so as well.

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#000000">
+    <link rel="manifest" href="/manifest.json">
+    <title>them apples</title>
+  </head>
+  <body>
+    <noscript>
+      You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root"></div>
+    
+    <script src="assets/js/bundle.js"></script>
+  </body>
+</html>
+`);
+});
+
+module.exports = router;
