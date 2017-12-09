2e1f872caead79f12da3f1ba789ad44c7d1433ec
mAAdhaTTah <jamesorodig@gmail.com>
2017-12-09 14:53:16 -0500

    Switch to valtech eslint configuration
    Will need to update some files' style in order to get the build passing.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    plugins: ['react'],
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+            impliedStrict: true,
+            experimentalObjectRestSpread: true
+        },
+    },
+    env: {
+        es6: true,
+        node: true,
+        browser: true
+    },
+    extends: 'valtech',
+    rules: {
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+    }
+};
