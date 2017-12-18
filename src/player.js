1052670093ddedc0b99168d9bf0454a0f65fa8d7
Daryl Keeter <Daryl.Keeter@valtech.com>
2017-12-18 18:03:17 -0500

    Add Sprite to Player model
    This uses react-game-kit’s sprite model and some basic defaults for the
    props.

--- src/player.js
+++ src/player.js
@@ -1,28 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Sprite } from 'react-game-kit';
 
 class Player extends React.Component {
 
-    constructor() {
-        super();
-
+    constructor(props) {
+        super(props);
+        const id = props.id || 0;
         this.state = {
-            id: 0,
-            name: '',
-            positionX: 0,
-            positionY: 0,
-            spritePath: '',
-            spriteWidth: 0,
-            spriteHeight: 0
-        }
+            id: id,
+            name: props.name || 'player-' + id,
+            positionX: props.positionX || 0,
+            positionY: props.positionY || 0,
+            spritePath: props.spritePath || '',
+            sizeMultiple: props.sizeMultiple || 1,
+            spriteTileWidth: props.spriteTileWidth || 64,
+            spriteTileHeight: props.spriteTileHeight || 64,
+            spriteRowState: props.spriteRowState || 0,
+            spriteAnimationSequence: props.spriteAnimationSequence || [9, 9, 0, 4, 5]
+        };
     }
 
     render() {
         return (
-            <div>
-                <img
+            <div id={'player-' + this.state.id} className='player-sprite-container'>
+                <Sprite
+                    repeat={true}
                     src={this.state.spritePath}
-                    height={this.state.spriteHeight}
-                    width={this.state.spriteWidth}
+                    scale={this.state.sizeMultiple}
+                    state={this.state.spriteRowState}
+                    steps={this.state.spriteAnimationSequence}
+                    tileHeight={this.state.spriteTileHeight}
+                    tileWidth={this.state.spriteTileWidth}
                 />
                 <p>{this.state.name}</p>
             </div>
